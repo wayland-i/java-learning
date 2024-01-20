f70ec27527fb7043efb9283839b18bb8bd57f0bb
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 16:49:37 -0500

    feature: number format

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,11 +1,13 @@
 import java.awt.*;
-import java.util.Arrays;
-import java.util.Date;
+import java.text.NumberFormat;
+
 
 public class Main {
     public static void main(String[] args) {
-        // Math is a built-in Class that provides you with mathematical methods
-        int result = (int) Math.round(Math.random() * 100);
+
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        String result = currency.format( 1234567.891);
+
         System.out.println(result);
     }
 }
