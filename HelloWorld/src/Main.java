2fad7e23b9a77b8541d88ba425502823f81eadf6
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 16:15:32 -0500

    feature: type conversion

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,8 +4,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        final float PI = 3.14F; //final is the keyword for const, constants are conventionally capitalized in JAVA
-
+        double result = (double)10 / (double)3;
+        System.out.println(result);
     }
 }
 
