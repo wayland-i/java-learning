f2da08bf58987cf63ebad1a5efd5d14e80c2d9c3
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 17:43:20 -0500

    feature: first half of mortgage calculator project

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,14 +1,31 @@
 import java.awt.*;
 import java.text.NumberFormat;
+import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
 
-        NumberFormat currency = NumberFormat.getCurrencyInstance();
-        String result = currency.format( 1234567.891);
+        Scanner pricipalScan = new Scanner(System.in);
+        System.out.print("Principal: ");
+        int principal = pricipalScan.nextInt();
+//        System.out.print(principal);
 
-        System.out.println(result);
+        Scanner interestScan = new Scanner(System.in);
+        System.out.print("Annual Interest Rate: ");
+        float interest = interestScan.nextFloat();
+//        System.out.print(interest);
+
+
+        Scanner periodScan = new Scanner(System.in);
+        System.out.print("Period (Years): ");
+        int period = periodScan.nextInt();
+//        System.out.print(period);
+
+        String mortgage = "answer";
+
+
+        System.out.println("Mortgage: " + mortgage);
     }
 }
 
