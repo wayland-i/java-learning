ede5b9c218cc3ff1669deda1434dac35d6509557
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 16:24:51 -0500

    feature: implicit/explicit casting coercion

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,8 +4,17 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        double result = (double)10 / (double)3;
-        System.out.println(result);
+        //Implicit casting
+        // if there is no chance of data loss, implicit type conversion will happen
+        // byte > short > int > long
+        double x = 1.1;
+        double y = x + 2;
+        // explicit casting
+        String z = "1";
+        int a = Integer.parseInt(z) + 2;
+
+        System.out.println(y);
+        System.out.println(a);
     }
 }
 
