893a2d834f8c5401f2b18be3441aeab0c8566632
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 11:15:17 -0500

    feature: new Types added, old removed

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -2,7 +2,15 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+//        int age = 30;
+        byte age = 30;
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+        System.out.println(age);
+
     }
 }
 
