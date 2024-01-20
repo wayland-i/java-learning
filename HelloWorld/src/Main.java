72dc304d86a28c625c7c51d465d8aca46af5475c
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 11:49:33 -0500

    feature: pointers made simple

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,15 +1,14 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.awt.*;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello World");
-//        int age = 30;
-        byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-        System.out.println(age);
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        point1.x = 2;
+
+        System.out.println(point2);
+
 
     }
 }
