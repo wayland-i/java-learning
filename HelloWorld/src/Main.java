8bc03b35f2ff5cc57946d051620d6eecf34f84e5
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 16:39:07 -0500

    feature: Math Class

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,17 +4,9 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        //Implicit casting
-        // if there is no chance of data loss, implicit type conversion will happen
-        // byte > short > int > long
-        double x = 1.1;
-        double y = x + 2;
-        // explicit casting
-        String z = "1";
-        int a = Integer.parseInt(z) + 2;
-
-        System.out.println(y);
-        System.out.println(a);
+        // Math is a built-in Class that provides you with mathematical methods
+        int result = (int) Math.round(Math.random() * 100);
+        System.out.println(result);
     }
 }
 
