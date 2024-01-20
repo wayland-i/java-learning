879e1f55ac18de8288f82c7d0d4972516c498ec6
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 14:24:14 -0500

    feature: dereferencing reference type array

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,10 +1,16 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        String message = "c:\\Windows\\...";
-        System.out.println(message);
+        int [] numbers = new int[5];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        Arrays.toString(numbers);
+
+        System.out.println(numbers); //numbers needs to be 'dereferenced' or else you will just print an address in memory
+        System.out.println(Arrays.toString(numbers)); // this dereferences numbers
 
     }
 }
