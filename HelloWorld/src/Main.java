4880eb884fa40ebdc64ff26850119ee118edf785
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 14:28:54 -0500

    featrue: array sorting

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -4,13 +4,9 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        int [] numbers = new int[5];
-        numbers[0] = 1;
-        numbers[1] = 2;
-        Arrays.toString(numbers);
-
-        System.out.println(numbers); //numbers needs to be 'dereferenced' or else you will just print an address in memory
-        System.out.println(Arrays.toString(numbers)); // this dereferences numbers
+        int[] numbers = {2, 3, 5, 1, 4};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
 
     }
 }
