dc3014a6e9635ff7a142503fb2c6c5415eed42a3
Ian Iversen <ianiversen1@gmail.com>
2024-01-20 13:42:50 -0500

    feature: special/reserved chars in strings

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,13 +3,8 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-//        String message = new String("Hello World");
-        // This how you always deal with reference types in Java^^^
-        String message = "Hello World" + "!!";
-//        message.endsWith("!!");
-        // But java allows you to do this as a short hand^^, still a reference type though
-        System.out.println(message.length());
-
+        String message = "c:\\Windows\\...";
+        System.out.println(message);
 
     }
 }
