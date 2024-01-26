1b57dc05da3432f7feacc4350e08f649eb441a10
Ian Iversen <ianiversen1@gmail.com>
2024-01-25 21:33:54 -0500

    feature: fizzbuzz my solution

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,17 +5,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String role = "admin";
-        switch (role) {
-            case "admin":
-                System.out.println("You're an admin");
-                break;
-            case "moderator":
-                System.out.println("You're a moderator");
-                break;
-            default:
-                System.out.println("You're a guest");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number: ");
+        int num = scanner.nextInt();
+
+        if (num % 5 == 0 && num % 3 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (num % 5 == 0) {
+            System.out.println("Fizz");
+        } else if (num % 3 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(num);
         }
+
+
     }
 }
 
+// input "Number"
+// if num % 5 = 0 "Fizz"
+// if num % 3 = 0 "Buzz
+// if num both "FizzBuzz"
+// if num none of the above, just print num
