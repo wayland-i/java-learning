b8356c2554274ad3361a17e318a91d2d2a062ab6
Ian Iversen <ianiversen1@gmail.com>
2024-01-25 21:02:01 -0500

    feature: logical operators

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 1;
-        int y = 1;
+//        int temperature = 22;
+//        boolean isWarm = temperature > 20 && temperature < 30;
 
-        System.out.println(x != y);
+        boolean hasHighIncome = false;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+//        person is eligible if they either have high income or good credit and they additionally do not have a criminal record
 
+//        System.out.println(isWarm);
+        System.out.println(isEligible);
     }
 }
 
