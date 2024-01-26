ed846a90f58eeb22966584810c0ca795fcaa0f06
Ian Iversen <ianiversen1@gmail.com>
2024-01-25 21:21:51 -0500

    feature: switch example implemented in if statements first

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,9 +5,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int income = 120_000;
-        String className = income > 100_000 ? "First Class" : "Economy";
-        System.out.println(className);
+        String role = "moderator";
+        if (role == "admin") {
+            System.out.println("You're an admin");
+        } else if (role == "moderator") {
+            System.out.println("You're a moderator");
+        } else {
+            System.out.println("You're a guest");
+        }
     }
 }
 
