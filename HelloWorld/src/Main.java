849d2fe672b3ff288a9cae84614cdef52f029bb3
Ian Iversen <ianiversen1@gmail.com>
2024-01-25 21:50:18 -0500

    feature: while loops first example

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello World " + i);
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Hello World");
+            i++;
         }
 
     }
