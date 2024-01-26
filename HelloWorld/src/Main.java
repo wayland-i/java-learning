22bd3ae22a80ea0b4656889ac4f3982635a15f34
Ian Iversen <ianiversen1@gmail.com>
2024-01-25 21:53:41 -0500

    feature: while loops

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,16 +1,18 @@
 import java.awt.*;
 import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
-        int i = 0;
-        while (i < 5) {
-            System.out.println("Hello World");
-            i++;
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
         }
-
     }
 }
 
