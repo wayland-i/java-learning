f7e5f46a7aeee53b621034129ffee265f50ed569
Ian Iversen <ianiversen1@gmail.com>
2024-01-25 21:57:07 -0500

    feature: do while loops

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         String input = "";
-        while (!input.equals("quit")) {
+        Scanner scanner = new Scanner(System.in);
+        do {
             System.out.print("Input: ");
             input = scanner.next().toLowerCase();
             System.out.println(input);
-        }
+        } while (!input.equals("quit"));
     }
 }
 
