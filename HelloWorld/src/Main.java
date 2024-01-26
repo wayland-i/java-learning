2497fd33c84f1db0662b1587131aae385827d241
Ian Iversen <ianiversen1@gmail.com>
2024-01-25 21:14:58 -0500

    feature: simplifying if statements

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -5,15 +5,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int temp = 25;
-        if (temp > 30) {
-            System.out.println("It's a hot day");
-            System.out.println("Drink plenty of water");
-        } else if (temp > 20) {
-            System.out.println("It's a nice day");
-        } else {
-            System.out.println("It's cold");
-        }
+        int income = 120_000;
+        boolean hasHighIncome = (income > 100_000);
+        
+        System.out.println(hasHighIncome);
     }
 }
 
