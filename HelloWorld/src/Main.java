e22284a9a600ddaf1385d5ad700bb11bf7e0f2b6
Ian Iversen <ianiversen1@gmail.com>
2024-01-27 22:42:34 -0500

    feature: for each loop

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -6,15 +6,14 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String input = "";
-        while (true) {
-            System.out.print("Input: ");
-            input = scanner.next().toLowerCase();
-            if (input.equals("quit")) {
-                break;
-            }
-            System.out.println(input);
+        String[] fruits = { "Apple", "Mango", "Orange"};
+
+        for (int i = 0; i < fruits.length; i++) {
+            System.out.println(fruits[i]);
+        }
+
+        for (String fruit : fruits) {
+            System.out.println(fruit);
         }
     }
 }
