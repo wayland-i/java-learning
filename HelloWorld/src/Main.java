6d622c48e9ae803cbd1f803fb61835d89ecf2210
Ian Iversen <ianiversen1@gmail.com>
2024-01-27 23:54:30 -0500

    feature: mosh's solution

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -19,9 +19,8 @@ public class Main {
             principal = scanner.nextInt();
             if (principal >= 1_000 && principal <= 1_000_000) {
                 break;
-            } else {
-                System.out.println("Enter a number between 1,000 and 1,000,000.");
             }
+            System.out.println("Enter a number between 1,000 and 1,000,000.");
         }
 
         float annualInterest;
@@ -32,9 +31,8 @@ public class Main {
             if (annualInterest > 0 && annualInterest <= 30) {
                 monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
                 break;
-            } else {
-                System.out.println("Enter a value greater than 0 and less than or equal to 30.");
             }
+            System.out.println("Enter a value greater than 0 and less than or equal to 30.");
         }
 
 
@@ -46,9 +44,8 @@ public class Main {
             if (years >= 1 && years <= 30) {
                 numberOfPayments = years * MONTHS_IN_YEAR;
                 break;
-            } else {
-                System.out.println("Enter a vlaue between 1 and 30.");
             }
+                System.out.println("Enter a vlaue between 1 and 30.");
         }
 
 
