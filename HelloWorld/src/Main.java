b224f3863773f3adcf757fb4e20e013da4f48fdc
Ian Iversen <ianiversen1@gmail.com>
2024-01-27 22:57:12 -0500

    feature: pre-checkout commit

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -6,20 +6,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String[] fruits = { "Apple", "Mango", "Orange"};
-
-        for (int i = 0; i < fruits.length; i++) {
-            System.out.println(fruits[i]);
-        }
-
-        for (String fruit : fruits) {
-            System.out.println(fruit);
-        }
+    
     }
 }
-
-// input "Number"
-// if num % 5 = 0 "Fizz"
-// if num % 3 = 0 "Buzz
-// if num both "FizzBuzz"
-// if num none of the above, just print num
