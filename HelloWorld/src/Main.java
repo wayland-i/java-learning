2d90ba6662d365d6f8d524a451f38793c88547b9
Ian Iversen <ianiversen1@gmail.com>
2024-01-27 22:35:07 -0500

    feature: eliminate print after 'quit' entered as input

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -6,13 +6,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "";
         Scanner scanner = new Scanner(System.in);
-        do {
+        String input = "";
+        while (!input.equals("quit")) {
             System.out.print("Input: ");
             input = scanner.next().toLowerCase();
-            System.out.println(input);
-        } while (!input.equals("quit"));
+            if (!input.equals("quit")) {
+                System.out.println(input);
+            }
+        }
     }
 }
 
